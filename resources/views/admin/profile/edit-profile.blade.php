625bcee605f233398f6f039c1b0f895c3663c1eb
Rafael Souza Borges <rafael.borges@basis.com.br>
2020-01-24 14:03:02 -0300

    Arquivos Iniciais

--- resources/views/admin/profile/edit-profile.blade.php
+++ resources/views/admin/profile/edit-profile.blade.php
@@ -11,7 +11,7 @@
             <profile-edit-profile-form
                 :action="'{{ url('admin/profile') }}'"
                 :data="{{ $adminUser->toJson() }}"
-                
+
                 inline-template>
 
                 <form class="form-horizontal form-edit" method="post" @submit.prevent="onSubmit" :action="action">
@@ -32,42 +32,72 @@
                                 </div>
                             </div>
                             <div class="col-md-8">
-                                <div class="form-group row align-items-center" :class="{'has-danger': errors.has('name'), 'has-success': fields.name && fields.name.valid }">
-                                    <label for="name" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-3'">{{ trans('admin.admin-user.columns.name') }}</label>
+                                <div class="form-group row align-items-center"
+                                     :class="{'has-danger': errors.has('name'), 'has-success': fields.name && fields.name.valid }">
+                                    <label for="name" class="col-form-label text-md-right"
+                                           :class="isFormLocalized ? 'col-md-4' : 'col-md-3'">{{ trans('admin.admin-user.columns.name') }}</label>
                                     <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-7'">
-                                        <input type="text" v-model="form.name" v-validate="''" @input="validate($event)" class="form-control" :class="{'form-control-danger': errors.has('name'), 'form-control-success': fields.name && fields.name.valid}" id="name" name="name" placeholder="{{ trans('admin.admin-user.columns.name') }}">
-                                        <div v-if="errors.has('name')" class="form-control-feedback form-text" v-cloak>@{{ errors.first('name') }}</div>
+                                        <input type="text" v-model="form.name" v-validate="''" @input="validate($event)"
+                                               class="form-control"
+                                               :class="{'form-control-danger': errors.has('name'), 'form-control-success': fields.name && fields.name.valid}"
+                                               id="name" name="name"
+                                               placeholder="{{ trans('admin.admin-user.columns.name') }}">
+                                        <div v-if="errors.has('name')" class="form-control-feedback form-text" v-cloak>
+                                            @{{ errors.first('name') }}
+                                        </div>
                                     </div>
                                 </div>
-                                
-                                <div class="form-group row align-items-center" :class="{'has-danger': errors.has('email'), 'has-success': fields.email && fields.email.valid }">
-                                    <label for="email" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-3'">{{ trans('admin.admin-user.columns.email') }}</label>
+
+                                <div class="form-group row align-items-center"
+                                     :class="{'has-danger': errors.has('email'), 'has-success': fields.email && fields.email.valid }">
+                                    <label for="email" class="col-form-label text-md-right"
+                                           :class="isFormLocalized ? 'col-md-4' : 'col-md-3'">{{ trans('admin.admin-user.columns.email') }}</label>
                                     <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-7'">
-                                        <input type="text" v-model="form.email" v-validate="'required|email'" @input="validate($event)" class="form-control" :class="{'form-control-danger': errors.has('email'), 'form-control-success': fields.email && fields.email.valid}" id="email" name="email" placeholder="{{ trans('admin.admin-user.columns.email') }}">
-                                        <div v-if="errors.has('email')" class="form-control-feedback form-text" v-cloak>@{{ errors.first('email') }}</div>
+                                        <input type="text" v-model="form.email" v-validate="'required|email'"
+                                               @input="validate($event)" class="form-control"
+                                               :class="{'form-control-danger': errors.has('email'), 'form-control-success': fields.email && fields.email.valid}"
+                                               id="email" name="email"
+                                               placeholder="{{ trans('admin.admin-user.columns.email') }}">
+                                        <div v-if="errors.has('email')" class="form-control-feedback form-text" v-cloak>
+                                            @{{ errors.first('email') }}
+                                        </div>
                                     </div>
                                 </div>
-                                
-                                <div class="form-group row" :class="{'has-danger': errors.has('is_admin'), 'has-success': fields.is_admin && fields.is_admin.valid }">
-                                    <div class="ml-md-auto" :class="isFormLocalized ? 'col-md-8' : 'col-md-9'">
-                                        <input class="form-check-input" id="is_admin" type="checkbox" v-model="form.is_admin" v-validate="''" data-vv-name="is_admin"  name="is_admin_fake_element">
-                                        <label class="form-check-label" for="is_admin">
-                                            {{ trans('admin.admin-user.columns.is_admin') }}
-                                        </label>
-                                        <input type="hidden" name="is_admin" :value="form.is_admin">
-                                        <div v-if="errors.has('is_admin')" class="form-control-feedback form-text" v-cloak>@{{ errors.first('is_admin') }}</div>
+
+                                @if(auth()->user()->is_admin == 1)
+                                    <div class="form-group row"
+                                         :class="{'has-danger': errors.has('is_admin'), 'has-success': fields.is_admin && fields.is_admin.valid }">
+                                        <div class="ml-md-auto" :class="isFormLocalized ? 'col-md-8' : 'col-md-9'">
+                                            <input class="form-check-input" id="is_admin" type="checkbox"
+                                                   v-model="form.is_admin" v-validate="''" data-vv-name="is_admin"
+                                                   name="is_admin_fake_element">
+                                            <label class="form-check-label" for="is_admin">
+                                                {{ trans('admin.admin-user.columns.is_admin') }}
+                                            </label>
+                                            <input type="hidden" name="is_admin" :value="form.is_admin">
+                                            <div v-if="errors.has('is_admin')" class="form-control-feedback form-text"
+                                                 v-cloak>@{{ errors.first('is_admin') }}
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                
-                                <div class="form-group row align-items-center" :class="{'has-danger': errors.has('language'), 'has-success': fields.language && fields.language.valid }">
-                                    <label for="language" class="col-form-label text-md-right" :class="isFormLocalized ? 'col-md-4' : 'col-md-3'">{{ trans('admin.admin-user.columns.language') }}</label>
+                                @endif
+
+                                <div class="form-group row align-items-center"
+                                     :class="{'has-danger': errors.has('language'), 'has-success': fields.language && fields.language.valid }">
+                                    <label for="language" class="col-form-label text-md-right"
+                                           :class="isFormLocalized ? 'col-md-4' : 'col-md-3'">{{ trans('admin.admin-user.columns.language') }}</label>
                                     <div :class="isFormLocalized ? 'col-md-4' : 'col-md-9 col-xl-7'">
-                                        <multiselect v-model="form.language" placeholder="{{ trans('brackets/admin-ui::admin.forms.select_an_option') }}" :options="{{ $locales->toJson() }}" open-direction="bottom"></multiselect>
-                                        <div v-if="errors.has('language')" class="form-control-feedback form-text" v-cloak>@{{ errors.first('language') }}</div>
+                                        <multiselect v-model="form.language"
+                                                     placeholder="{{ trans('brackets/admin-ui::admin.forms.select_an_option') }}"
+                                                     :options="{{ $locales->toJson() }}"
+                                                     open-direction="bottom"></multiselect>
+                                        <div v-if="errors.has('language')" class="form-control-feedback form-text"
+                                             v-cloak>@{{ errors.first('language') }}
+                                        </div>
                                     </div>
                                 </div>
-                                
-                                
+
+
                             </div>
                         </div>
 
